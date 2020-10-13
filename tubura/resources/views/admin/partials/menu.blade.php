ae0db72828281805cae80c7e63f84236d7aae228
HABUMUGISHA0784643608 <jeanclaudehabumugish81@gmail.com>
2020-10-13 09:24:08 +0200

    initial commit

---
+++ tubura/resources/views/admin/partials/menu.blade.php
@@ -0,0 +1,74 @@
+<div class="left-side-bar">
+    <div class="brand-logo">
+        <a href="index.html">
+            <img src="vendors/images/deskapp-logo.svg" alt="" class="dark-logo">
+            <img src="vendors/images/deskapp-logo-white.svg" alt="" class="light-logo">
+        </a>
+        <div class="close-sidebar" data-toggle="left-sidebar-close">
+            <i class="ion-close-round"></i>
+        </div>
+    </div>
+    <div class="menu-block customscroll">
+        <div class="sidebar-menu">
+            <ul id="accordion-menu">
+            
+
+
+                <li>
+                    <a href="#" class="dropdown-toggle no-arrow">
+                        <span class="micon dw dw-house-1"></span><span class="mtext">Home</span>
+                    </a>
+                </li>
+                <li class="dropdown">
+                    <a href="javascript:;" class="dropdown-toggle">
+                        <span class="micon dw dw-right-arrow1"></span><span class="mtext">My Lost Documents</span>
+                 
+                      
+                    </a>
+                    <ul class="submenu">
+                    <li><a href="#">All Lost document</a></li>
+                        <li><a href="#">New Lost document </a></li>
+                    
+                    </ul>
+                </li>
+
+
+               
+                <li class="dropdown">
+                    <a href="javascript:;" class="dropdown-toggle">
+                        <span class="micon dw dw-right-arrow1"></span><span class="mtext">My Found Documents</span>
+                 
+                      
+                    </a>
+                    <ul class="submenu">
+                        <li><a href="#">Found document</a></li>
+                            <li><a href="#">New found document </a></li>
+                        
+                        </ul>
+                    
+                </li>
+
+
+
+                <li>
+                    <a href="#" class="dropdown-toggle no-arrow">
+                        <span class="micon dw dw-right-arrow1"></span><span class="mtext">My invoces</span>
+                    </a>
+                </li>
+
+
+
+            
+              
+                
+            
+        
+                
+            
+                
+        
+            </ul>
+        </div>
+    </div>
+</div>
+<div class="mobile-menu-overlay"></div>
